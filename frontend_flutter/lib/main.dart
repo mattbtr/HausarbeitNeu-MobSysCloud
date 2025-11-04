6c78b5a2d6c64bff59d878bc60bf32f9c36cc771
mattbtr <m.beiterhhh@googlemail.com>
2025-11-04 20:09:13 +0100

    env für firebase secrets

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart'; // import für Design UI-Toolkit von Flutter (Widgets, Themes etc.)
 import 'package:firebase_core/firebase_core.dart'; // Firebase Core Package für Firebase App in Flutter
-import 'firebase_options.dart';   // enthält Firebase-Prohjekt-Konfiguration
+//import 'firebase_options.dart';   // enthält Firebase-Prohjekt-Konfiguration
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-//import 'firebase_options_env.dart'; // ersetzt firebase_options.dart
+import 'firebase_options_env.dart'; // ersetzt firebase_options.dart
 import 'package:logger/logger.dart';
 import 'app.dart';
 
@@ -12,14 +12,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Sorgt dafür, dass alle Widgets und Dienste bevor Firebase initialisiert wird.
 
-  //await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: '.env');
 
   try {
     if (Firebase.apps.isEmpty) {
       await Firebase.initializeApp(
         // Firebase Projekt initialisieren
-        options: DefaultFirebaseOptions.currentPlatform,
-        //options: FirebaseOptionsEnv.currentPlatform,
+        //options: DefaultFirebaseOptions.currentPlatform,
+        options: FirebaseOptionsEnv.currentPlatform,
         // Verwendet die manuelle Konfiguration aus firebase_options_env.dart die die Werte aus .env nimmt
         // (enthält u. a. API-Key, Projekt-ID etc.)
       );
